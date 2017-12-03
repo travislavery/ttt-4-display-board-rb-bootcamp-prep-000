3a24effd71fc2301f531e5116be5a8408564d3f7
Travis Lavery <travisalavery@gmail.com>
2017-12-03 23:46:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,13 +2,9 @@
 # out the current state.
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board(board)
-  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
-  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
-  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
-  interRow = "-----------"
-  print row1 + "\n"
-  print interRow + "\n"
-  print row2 + "\n"
-  print interRow + "\n"
-  print row3 + "\n"
+  puts row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
 end
